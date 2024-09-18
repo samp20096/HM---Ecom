ab7005332e417383685c8bc7b1fb1b7d4a585b12
samp20096 <samp20096@gmail.com>
2024-09-18 18:02:30 +0300

    Update bonus_01.py

--- MW-06/bonus_01.py
+++ MW-06/bonus_01.py
@@ -3,13 +3,15 @@
 number: int = int(input("Put a number: "))
 count: int = 0
 
-while number % 7 == 0:
-    if number % 11 != 0:
-        count += 1
-        number = int(input("Put another number: "))
-    else:
+while number % 7 == 0 or number % 11 == 0:
+    if number % 11 == 0:
         print("Your number is not in sequence of % 7 == 0")
         break
+    else:
+        count += 1
+        number = int(input("Put another number: "))
 
 else:
     print(f"Your number is not % 7 == 0 \nYou tried for {count} times")
+
+# End
